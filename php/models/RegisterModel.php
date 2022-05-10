2395d5ddb352d8d8c210595d2e7423c892e40b6c
Namvo-1304 <55818180+Namvo-1304@users.noreply.github.com>
2022-05-10 08:42:44 -0700

    Apply more Validate
    Apply more Validate

--- php/models/RegisterModel.php
+++ php/models/RegisterModel.php
@@ -3,7 +3,21 @@
 namespace app\models;
 
 #Define all attribute of that model you are going to use it
-class RegisterModel
-{
+use app\core\Model;
 
+class RegisterModel extends Model
+{
+    public string $email;
+    public string $pass;
+    public function register(){
+        echo "Creating new user";
+    }
+    public function rules(): array
+    {
+        // TODO: Implement rules() method.
+        return [
+            'email'=>[self::RULE_REQUIRED,self::RULE_EMAIL],
+            'pass'=>[self::RULE_REQUIRED]
+        ];
+    }
 }
--- php/views/register.php
+++ php/views/register.php
@@ -3,11 +3,11 @@
 <form action="" method="post">
     <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="text" class="form-control" aria-describedby="emailHelp" name="Email">
+        <input type="text" class="form-control" aria-describedby="emailHelp" name="email">
     </div>
     <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="text" class="form-control" name="Pass">
+        <input type="text" class="form-control" name="pass">
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
